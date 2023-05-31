f1c5a69b8130ed4d3b6eacee7c90a58ad7430745
Fernando Seguins <seu@email.com>
2023-05-30 23:08:17 -0300

    components createds styles

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit {
 
+  @Input()
+  photoCover:string="https://files.tecnoblog.net/wp-content/uploads/2021/04/Qual-a-ordem-cronologica-dos-filmes-do-Batman-Deny-Freeman-Flickr.jpg"
+  cardTitle:string="Batman volta dos mortos"
+  cardDescription:string="Elel morreu ou foi tudo fake?"
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
+
+  
 }
